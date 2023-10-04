15521eb559c5032882eff68e90daeae95fe8b549
gabitoher2506 <144975521+gabitoher2506@users.noreply.github.com>
2023-10-03 22:24:51 -0600

    Push1

---
+++ PaginaGabo/src/main/java/pagina/demo/PaginaGaboApplication.java
@@ -0,0 +1,13 @@
+package pagina.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaGaboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaGaboApplication.class, args);
+	}
+
+}
---
+++ PaginaGabo/src/test/java/pagina/demo/PaginaGaboApplicationTests.java
@@ -0,0 +1,13 @@
+package pagina.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginaGaboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
